2b31eaacb0888513c35ff7408b561f51a00fb165
Yaroslav Opanasenko <yarolav.opanasenko99@gmail.com>
2018-10-30 19:15:34 +0200

    op

--- LSTM/keras_lstm.py
+++ LSTM/keras_lstm.py
@@ -116,10 +116,10 @@ with tf.device("/cpu:0"):
     model.add(Embedding(vocabulary, hidden_size, input_length=num_steps))
     model.add(LSTM(hidden_size, return_sequences=True))
     model.add(LSTM(hidden_size, return_sequences=True))
-	if use_dropout:
-    	model.add(Dropout(0.5))
-	model.add(TimeDistributed(Dense(vocabulary)))
-	model.add(Activation('softmax'))
+    if use_dropout:
+        model.add(Dropout(0.5))
+    model.add(TimeDistributed(Dense(vocabulary)))
+    model.add(Activation('softmax'))
 # make the model parallel
 model = multi_gpu_model(model, gpus=4)
 
